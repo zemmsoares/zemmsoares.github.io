eeee71b12ff4e9975e934a78fe8be738c76e644f
zemmsoares <zemmsoares@gmail.com>
2022-11-24 15:39:37 +0000

    b done

--- js/terminal.js
+++ js/terminal.js
@@ -14,26 +14,26 @@ $(function () {
   function help(term) {
     term.echo(
       "Available commands: \n\n" +
+        "- pfetch              A pretty system information tool written in POSIX sh.\n" +
         "- cat [file]          Concatenate FILE(s) to standard output.\n" +
-        "- ls                  List information about the FILEs (the current directory by default).\n" +
-        "- pfetch              A pretty system information tool written in POSIX sh.\n"
+        "- ls                  List information about the FILEs (the current directory by default).\n"
     );
   }
 
   function list(term) {
     //term.echo(dir.color);
-    term.echo("[[;lime;]teste.sh] " + "about " + "links ");
+    term.echo(/*"[[;lime;]teste.sh] " + */ "[[;lime;]about] " + "links ");
   }
 
   function cat(term, file) {
     if (file == "about") {
-      //term.echo("bababa baabab baba bababa");
-
+      term.echo("About info to do");
+      /*
       term.echo(
         '<img src="https://avatars.githubusercontent.com/u/38134965?v=4"/>',
         { raw: true }
       );
-
+*/
       /*
       term.echo(
         "<div class='container-parent'>" +
@@ -45,9 +45,9 @@ $(function () {
           { raw: true }
       );
       */
-    } else if (file == "teste.sh") {
+    } /*else if (file == "teste.sh") {
       term.echo("#!/usr/bin/env bash\n\n" + "sudo rm -rf");
-    } else if (file == "links") {
+    }*/ else if (file == "links") {
       term.echo(
         "[[!;;;;https://github.com/zemmsoares]Github]\n" +
           "[[!;;;;https://www.linkedin.com/in/zemmsoares/]Linkedin]"
@@ -60,14 +60,14 @@ $(function () {
   function pfetch(term) {
     term.echo(
       "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;zm@archlap</p>" +
-        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;os     Arch Linux</p>" +
-        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;host   VivoBook_ASUSLaptop X530UFO 1.0</p>" +
-        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;kernel 6.0.9-arch1-1</p>" +
-        "<p class='line'>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;,,&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;uptime " +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>os:</b></span>     Arch Linux + Windows for Adobe</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>host:</b></span>   VivoBook_ASUSLaptop X530UFO 1.0</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>cv:</b></span> 6.0.9-arch1-1</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;,,&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>uptime:</b></span> " +
         time() +
         "</p>" +
-        "<p class='line'>&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;|&nbsp;&nbsp;-\\&nbsp;&nbsp;&nbsp;&nbsp;pkgs   722</p>" +
-        "<p class='line'>&nbsp;/_-''&nbsp;&nbsp;&nbsp;&nbsp;''-_\\&nbsp;&nbsp;&nbsp;memory 6804M / 15883M</p>",
+        "<p class='line'>&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;|&nbsp;&nbsp;-\\&nbsp;&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>pkgs:</b></span>   722</p>" +
+        "<p class='line'>&nbsp;/_-''&nbsp;&nbsp;&nbsp;&nbsp;''-_\\&nbsp;&nbsp;&nbsp;<span class='pfetch__title'><b>memory:</b></span> 6804M / 15883M</p>",
       { raw: true }
     );
   }
@@ -105,7 +105,7 @@ $(function () {
       //    return false;
       //  }
       //},
-      completion: ["about", "teste.sh", "links"],
+      completion: ["about", "links"],
     }
   );
 });
