9eab5f37715c49d40e405813c75c5b67514e4870
zemmsoares <zemmsoares@gmail.com>
2022-11-24 14:07:11 +0000

    fetch info

--- js/terminal.js
+++ js/terminal.js
@@ -12,7 +12,12 @@ $(function () {
   }
 
   function help(term) {
-    term.echo("Available commands: \n\n" + "- cat\n" + "- ls\n");
+    term.echo(
+      "Available commands: \n\n" +
+        "- cat [file]          Concatenate FILE(s) to standard output.\n" +
+        "- ls                  List information about the FILEs (the current directory by default).\n" +
+        "- pfetch              A pretty system information tool written in POSIX sh.\n"
+    );
   }
 
   function list(term) {
@@ -22,11 +27,24 @@ $(function () {
 
   function cat(term, file) {
     if (file == "about") {
-      term.echo("bababa baabab baba bababa");
+      //term.echo("bababa baabab baba bababa");
+
       term.echo(
         '<img src="https://avatars.githubusercontent.com/u/38134965?v=4"/>',
         { raw: true }
       );
+
+      /*
+      term.echo(
+        "<div class='container-parent'>" +
+          "<div class='col'>" +
+          '<img src="https://avatars.githubusercontent.com/u/38134965?v=4"/>' +
+          "</div>" +
+          "<div class='col'>" +
+          "</div>" +
+          { raw: true }
+      );
+      */
     } else if (file == "teste.sh") {
       term.echo("#!/usr/bin/env bash\n\n" + "sudo rm -rf");
     } else if (file == "links") {
@@ -39,6 +57,20 @@ $(function () {
     }
   }
 
+  function pfetch(term) {
+    term.echo(
+      "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;zm@archlap</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;os     Arch Linux</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;host   VivoBook_ASUSLaptop X530UFO 1.0</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;kernel 6.0.9-arch1-1</p>" +
+        "<p class='line'>&nbsp;&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;,,&nbsp;&nbsp;&nbsp;\\&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;uptime 3d 2h 32m</p>" +
+        "<p class='line'>&nbsp;&nbsp;/&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;|&nbsp;&nbsp;-\\&nbsp;&nbsp;&nbsp;&nbsp;pkgs   722</p>" +
+        "<p class='line'>&nbsp;/_-''&nbsp;&nbsp;&nbsp;&nbsp;''-_\\&nbsp;&nbsp;&nbsp;memory 6804M / 15883M</p>",
+
+      { raw: true }
+    );
+  }
+
   $("body").terminal(
     {
       help: function () {
@@ -49,6 +81,9 @@ $(function () {
       },
       cat: function (file) {
         cat(this, file);
+      },
+      pfetch: function () {
+        pfetch(this);
       },
     },
     {
